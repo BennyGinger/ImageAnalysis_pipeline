195afe141476228e8b171dc0dc3cde014865f631
Benoit Roux <benoit.roux@gmail.com>
2024-03-21 16:25:46 +0000

    ImSeq refactor final

--- pipeline/settings/settings_dict.py
+++ pipeline/settings/settings_dict.py
@@ -1,11 +1,11 @@
 settings = {
-    "input_folder": '/home/Test_images/tiff/Run2',
+    "input_folder": '/home/Test_images/nd2/Run2',
     "init":{"active_channel_list": ['GFP','RFP'],
             'full_channel_list':[],
             "overwrite": True},
     
     
-    "run_bg_sub": True,
+    "run_bg_sub": False,
     "bg_sub": {"sigma": 0.0,
                "size": 7,
                "overwrite": False},
