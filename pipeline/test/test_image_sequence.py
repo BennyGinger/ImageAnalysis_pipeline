1cb08880a021cbc4633dbae77ee4f90b7d2bd7e9
Benoit Roux <benoit.roux@gmail.com>
2024-04-06 22:07:03 +0000

    Refactored test_metadata.py and test_inage_seq

--- pipeline/test/test_image_sequence.py
+++ pipeline/test/test_image_sequence.py
@@ -1,60 +1,94 @@
 
-# import numpy as np
-# from tifffile import imwrite
-# from pre_process.image_sequence import get_img_params_lst, expand_array_dim
+from os import mkdir
+import numpy as np
+from tifffile import imwrite, imread
+from pre_process.image_sequence import expand_array_dim, write_array, get_img_params_lst
 
-# def test_create_img_name_list():
-#     meta_dict = {
-#         'n_series': 2,
-#         'n_frames': 3,
-#         'n_slices': 4,
-#         'active_channel_list': ['GFP', 'RFP']
-#     }
-#     expected_output = [
-#         'GFP_s01_f0001_z0001', 'GFP_s01_f0001_z0002', 'GFP_s01_f0001_z0003', 'GFP_s01_f0001_z0004',
-#         'GFP_s01_f0002_z0001', 'GFP_s01_f0002_z0002', 'GFP_s01_f0002_z0003', 'GFP_s01_f0002_z0004',
-#         'GFP_s01_f0003_z0001', 'GFP_s01_f0003_z0002', 'GFP_s01_f0003_z0003', 'GFP_s01_f0003_z0004',
-#         'RFP_s01_f0001_z0001', 'RFP_s01_f0001_z0002', 'RFP_s01_f0001_z0003', 'RFP_s01_f0001_z0004',
-#         'RFP_s01_f0002_z0001', 'RFP_s01_f0002_z0002', 'RFP_s01_f0002_z0003', 'RFP_s01_f0002_z0004',
-#         'RFP_s01_f0003_z0001', 'RFP_s01_f0003_z0002', 'RFP_s01_f0003_z0003', 'RFP_s01_f0003_z0004',
-#         'GFP_s02_f0001_z0001', 'GFP_s02_f0001_z0002', 'GFP_s02_f0001_z0003', 'GFP_s02_f0001_z0004',
-#         'GFP_s02_f0002_z0001', 'GFP_s02_f0002_z0002', 'GFP_s02_f0002_z0003', 'GFP_s02_f0002_z0004',
-#         'GFP_s02_f0003_z0001', 'GFP_s02_f0003_z0002', 'GFP_s02_f0003_z0003', 'GFP_s02_f0003_z0004',
-#         'RFP_s02_f0001_z0001', 'RFP_s02_f0001_z0002', 'RFP_s02_f0001_z0003', 'RFP_s02_f0001_z0004',
-#         'RFP_s02_f0002_z0001', 'RFP_s02_f0002_z0002', 'RFP_s02_f0002_z0003', 'RFP_s02_f0002_z0004',
-#         'RFP_s02_f0003_z0001', 'RFP_s02_f0003_z0002', 'RFP_s02_f0003_z0003', 'RFP_s02_f0003_z0004'
-#     ]
-#     assert sorted(get_img_params_lst(meta_dict)) == sorted(expected_output)
-
-# def test_expand_dim_tif(tmpdir):
-#     # Create a temporary 3D numpy array
-#     img_data = np.random.rand(3, 10, 10)
-
-#     # Save the array to a temporary image file
-#     img_path = tmpdir.join("image.tif")
-#     imwrite(str(img_path), img_data)
-
-#     # Test case 1: All axes are present
-#     axes = 'CYX'
-#     expanded_data = expand_array_dim(img_path, axes)
     
-#     # Check that the output is a 5D numpy array
-#     assert isinstance(expanded_data, np.ndarray)
-#     assert expanded_data.ndim == 5
-#     assert img_data.shape[0] == expanded_data.shape[2]
-
-#     # Test case 2: Missing axes
-#     axes = 'TYX'
-#     expanded_data = expand_array_dim(img_path, axes)
-#     assert isinstance(expanded_data, np.ndarray)
-#     assert expanded_data.ndim == 5
-#     assert img_data.shape[0] == expanded_data.shape[0]
-
-#     # Test case 3: Missing multiple axes
-#     axes = 'ZYX'
-#     expanded_data = expand_array_dim(img_path, axes)
-#     assert isinstance(expanded_data, np.ndarray)
-#     assert expanded_data.ndim == 5
-#     assert img_data.shape[0] == expanded_data.shape[1]
+def test_write_array(tmpdir):
+    # Create a temporary metadata dictionary
+    metadata = {
+        'exp_path_list': [tmpdir],
+        'um_per_pixel': (0.1,0.1),
+        'interval_sec': 1.0,}
     
+    # Create a temporary folder to save the image
+    mkdir(tmpdir.join("Images"))
+    
+    # Create a temporary 3D numpy array
+    array = np.random.randint(1, size=(3, 10, 10))
+
+    # Create a temporary input data dictionary
+    input_data = {
+        'metadata': metadata,
+        'serie': 0,
+        'img_name': 'image1',
+        'array': array,
+        'array_slice': 1}
+
+    # Call the write_array function
+    write_array(input_data)
+
+    # Read the saved image
+    expected_save_path = tmpdir.join("Images","image1.tif")
+    saved_image = imread(str(expected_save_path))
+    
+    # Check that the saved image is equal to the input array slice
+    assert np.array_equal(saved_image, array[1])
+
+def test_get_img_params_lst():
+    meta_dict = {
+        'n_series': 1,
+        'n_frames': 3,
+        'n_slices': 4,
+        'active_channel_list': ['GFP', 'RFP'],
+        'full_channel_list': ['GFP', 'RFP']
+    }
+    expected_output = [{'array_slice': (0, 0, 0, 0), 'serie': 0, 'img_name': 'GFP_s01_f0001_z0001'},
+                       {'array_slice': (0, 0, 0, 1), 'serie': 0, 'img_name': 'RFP_s01_f0001_z0001'},
+                       {'array_slice': (0, 0, 1, 0), 'serie': 0, 'img_name': 'GFP_s01_f0001_z0002'},
+                       {'array_slice': (0, 0, 1, 1), 'serie': 0, 'img_name': 'RFP_s01_f0001_z0002'},
+                       {'array_slice': (0, 0, 2, 0), 'serie': 0, 'img_name': 'GFP_s01_f0001_z0003'},
+                       {'array_slice': (0, 0, 2, 1), 'serie': 0, 'img_name': 'RFP_s01_f0001_z0003'},
+                       {'array_slice': (0, 0, 3, 0), 'serie': 0, 'img_name': 'GFP_s01_f0001_z0004'},
+                       {'array_slice': (0, 0, 3, 1), 'serie': 0, 'img_name': 'RFP_s01_f0001_z0004'},
+                       {'array_slice': (0, 1, 0, 0), 'serie': 0, 'img_name': 'GFP_s01_f0002_z0001'},
+                       {'array_slice': (0, 1, 0, 1), 'serie': 0, 'img_name': 'RFP_s01_f0002_z0001'},
+                       {'array_slice': (0, 1, 1, 0), 'serie': 0, 'img_name': 'GFP_s01_f0002_z0002'},
+                       {'array_slice': (0, 1, 1, 1), 'serie': 0, 'img_name': 'RFP_s01_f0002_z0002'},
+                       {'array_slice': (0, 1, 2, 0), 'serie': 0, 'img_name': 'GFP_s01_f0002_z0003'},
+                       {'array_slice': (0, 1, 2, 1), 'serie': 0, 'img_name': 'RFP_s01_f0002_z0003'},
+                       {'array_slice': (0, 1, 3, 0), 'serie': 0, 'img_name': 'GFP_s01_f0002_z0004'},
+                       {'array_slice': (0, 1, 3, 1), 'serie': 0, 'img_name': 'RFP_s01_f0002_z0004'},
+                       {'array_slice': (0, 2, 0, 0), 'serie': 0, 'img_name': 'GFP_s01_f0003_z0001'},
+                       {'array_slice': (0, 2, 0, 1), 'serie': 0, 'img_name': 'RFP_s01_f0003_z0001'},
+                       {'array_slice': (0, 2, 1, 0), 'serie': 0, 'img_name': 'GFP_s01_f0003_z0002'},
+                       {'array_slice': (0, 2, 1, 1), 'serie': 0, 'img_name': 'RFP_s01_f0003_z0002'},
+                       {'array_slice': (0, 2, 2, 0), 'serie': 0, 'img_name': 'GFP_s01_f0003_z0003'},
+                       {'array_slice': (0, 2, 2, 1), 'serie': 0, 'img_name': 'RFP_s01_f0003_z0003'},
+                       {'array_slice': (0, 2, 3, 0), 'serie': 0, 'img_name': 'GFP_s01_f0003_z0004'},
+                       {'array_slice': (0, 2, 3, 1), 'serie': 0, 'img_name': 'RFP_s01_f0003_z0004'}]
+    
+    assert sorted(get_img_params_lst(meta_dict),key=lambda d: d['img_name']) == sorted(expected_output,key=lambda d: d['img_name'])
+
+def test_expand_dim_tif():
+    # Test case 1: All axes are present
+    # Create a temporary numpy array
+    img_data = np.random.rand(2, 2, 4, 3, 10, 10)
+
+    axes = 'PTZCYX'
+    expanded_data = expand_array_dim(img_data, axes)
+    print(expanded_data.shape)
+    # Check that the output is a 5D numpy array
+    assert isinstance(expanded_data, np.ndarray)
+    assert expanded_data.ndim == 6
+    assert img_data.shape == expanded_data.shape
+
+    # Test case 2: Missing axes
+    img_data = np.random.rand(2, 10, 10)
+    axes = 'TYX'
+    expanded_data = expand_array_dim(img_data, axes)
+    assert isinstance(expanded_data, np.ndarray)
+    assert expanded_data.ndim == 6
+    assert expanded_data.shape[0] == expanded_data.shape[2] == 1
     
--- pipeline/test/test_metadata.py
+++ pipeline/test/test_metadata.py
@@ -1,49 +1,39 @@
-# import pytest
-# from pre_process.metadata import get_tif_meta, get_ND2_meta, update_channel_names
+import pytest
+from pre_process.metadata import get_tif_meta, get_ND2_meta, update_channel_names
 
-# LIST_TIF = ['/home/Test_images/tiff/Run1/c1z25t25v1_tif.tif',
-#             '/home/Test_images/tiff/Run2/c2z25t23v1_tif.tif',
-#             '/home/Test_images/tiff/Run4/c4z1t91v1.tif']
+LIST_TIF = ['/home/Test_images/tiff/Run1/c1z25t25v1_tif.tif',
+            '/home/Test_images/tiff/Run2/c2z25t23v1_tif.tif',
+            '/home/Test_images/tiff/Run4/c4z1t91v1.tif']
 
-# LIST_ND2 = ['/home/Test_images/nd2/Run1/c1z25t25v1_nd2.nd2',
-#             '/home/Test_images/nd2/Run2/c2z25t23v1_nd2.nd2',
-#             '/home/Test_images/nd2/Run3/c3z1t1v3.nd2',
-#             '/home/Test_images/nd2/Run4/c4z1t91v1.nd2']
+LIST_ND2 = ['/home/Test_images/nd2/Run1/c1z25t25v1_nd2.nd2',
+            '/home/Test_images/nd2/Run2/c2z25t23v1_nd2.nd2',
+            '/home/Test_images/nd2/Run3/c3z1t1v3.nd2',
+            '/home/Test_images/nd2/Run4/c4z1t91v1.nd2']
 
-# @pytest.mark.parametrize('input_tif',LIST_TIF)
-# def test_get_tif_meta(input_tif):
-#     key_outputs = ['ImageWidth','ImageLength','frames','channels','slices','n_series','axes','finterval','file_type']
+@pytest.mark.parametrize('input_tif',LIST_TIF)
+def test_get_tif_meta(input_tif):
+    key_outputs = ['img_width','img_length','n_frames','full_n_channels','n_slices','axes','interval_sec']
     
-#     output_dict = get_tif_meta(input_tif)
-#     for key in key_outputs:
-#         assert key in output_dict
+    output_dict = get_tif_meta(input_tif)
+    for key in key_outputs:
+        assert key in output_dict
 
-# @pytest.mark.parametrize("input_nd2",LIST_ND2)  
-# def test_get_ND2_meta(input_nd2):
-#     key_output = ['x','y','t','c','z','v','pixel_microns','axes','file_type']
-#     output_dict = get_ND2_meta(input_nd2)
-#     for key in key_output:
-#         assert key in output_dict
+@pytest.mark.parametrize("input_nd2",LIST_ND2)  
+def test_get_ND2_meta(input_nd2):
+    key_output = ['full_n_channels', 'n_slices', 'n_frames', 'n_series', 'img_width', 'img_length']
+    output_dict = get_ND2_meta(input_nd2)
+    for key in key_output:
+        assert key in output_dict
         
-# # @pytest.mark.parametrize("input",LIST_ND2+LIST_TIF)
-# # def test_uniformize_meta(input):
-# #     meta_dict = get_ND2_meta(input) if input.endswith('.nd2') else get_tif_meta(input)
-# #     output_dict = uniformize_meta(meta_dict)
-# #     new_keys = ['img_width','img_length','n_frames','full_n_channels','n_slices','n_series','um_per_pixel','axes','interval_sec','file_type']
-# #     for key in output_dict.keys():
-# #         assert key in new_keys
-# #     assert isinstance(output_dict['interval_sec'],int)
-# #     assert isinstance(output_dict['um_per_pixel'],tuple)
-    
-# @pytest.mark.parametrize("input,output",
-#                          [([{'full_n_channels':2},[],[]],[['C1','C2'],['C1','C2']]),
-#                           ([{'full_n_channels':2},['GFP','RFP'],[]],[['GFP','RFP'],['GFP','RFP']]),
-#                           ([{'full_n_channels':2},['GFP','RFP'],['GFP','RFP']],[['GFP','RFP'],['GFP','RFP']]),
-#                           ([{'full_n_channels':2},['GFP','RFP'],['RFP','GFP']],[['GFP','RFP'],['GFP','RFP']]),
-#                           ([{'full_n_channels':2},['GFP','RFP','DAPI'],[]],[['C1','C2'],['C1','C2']])])
-# def test_update_channel_names(input,output):
-#     input_dict, active_channel_list, full_channel_list = input
-#     output_dict = update_channel_names(input_dict,active_channel_list,full_channel_list)
-#     out_active, out_full = output
-#     assert output_dict['active_channel_list'] == out_active
-#     assert output_dict['full_channel_list'] == out_full
+@pytest.mark.parametrize("input,output",
+                         [([{'full_n_channels':2},[],[]],[['C1','C2'],['C1','C2']]),
+                          ([{'full_n_channels':2},['GFP','RFP'],[]],[['GFP','RFP'],['GFP','RFP']]),
+                          ([{'full_n_channels':2},['GFP','RFP'],['GFP','RFP']],[['GFP','RFP'],['GFP','RFP']]),
+                          ([{'full_n_channels':2},['GFP','RFP'],['RFP','GFP']],[['GFP','RFP'],['GFP','RFP']]),
+                          ([{'full_n_channels':2},['GFP','RFP','DAPI'],[]],[['C1','C2'],['C1','C2']])])
+def test_update_channel_names(input,output):
+    input_dict, active_channel_list, full_channel_list = input
+    output_dict = update_channel_names(input_dict,active_channel_list,full_channel_list)
+    out_active, out_full = output
+    assert output_dict['active_channel_list'] == out_active
+    assert output_dict['full_channel_list'] == out_full
