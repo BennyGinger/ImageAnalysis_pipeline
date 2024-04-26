f1899f26d0f768795e34620a845b6b775c5a76f6
Benoit Roux <benoit.roux@gmail.com>
2024-04-26 13:55:34 +0000

    ImSeq works as module but the import path

--- pipeline/pre_process/image_sequence_copy.py
+++ pipeline/pre_process/image_sequence_copy.py
@@ -1,40 +1,36 @@
 from __future__ import annotations
-from os import PathLike, mkdir
-from os.path import join, getsize, exists
-import warnings
-from preprocess_utils import save_tif
+from os import PathLike
+from os.path import join, getsize
+from preprocess_utils import save_tif, create_save_folder
 from metadata import get_metadata
 
 from nd2 import ND2File
 from tifffile import imread
 import numpy as np
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 
 
 ################################## main function ###################################
-def process_img(img_path: PathLike, active_channel_list: list[str] = [], full_channel_list: list[str] = [])-> None:
+def process_img(img_path: PathLike, active_channel_list: list[str] = [], full_channel_list: list[str] = [])-> dict:
     """Determine which process fct to use depending on the file type and size of the img"""
     # Get file metadata
     meta_dict = get_metadata(img_path, active_channel_list, full_channel_list)
     
-    # If image is tiff, then process the array
-    if meta_dict['file_type'] == '.tif':
-        # Get array
-        array = imread(img_path)
-        process_img_array(array,meta_dict)
-        return
+    # If nd2 file is bigger than 20 GB, then process the ND2File obj frame by frame
+    if getsize(img_path) > 20e9 and meta_dict['file_type'] == '.nd2':
+        process_img_obj(meta_dict)
+        return meta_dict
     
-    # Else process nd2 file
-    # If img lower than 20 GB then process array
-    if getsize(img_path) < 20e9:
-        # Get array
+    # Else get the array (including nd2 and tiff) and process it
+    if meta_dict['file_type'] == '.tif':
+        array = imread(img_path)
+    else:
         with ND2File(img_path) as nd_obj:
             array = nd_obj.asarray()
-        process_img_array(array,meta_dict)
-        return
-    # If more than 20 GB
-    process_img_obj(meta_dict)
-
+    # Process the array
+    process_img_array(array,meta_dict)
+    return meta_dict
+    
 def process_img_array(array: np.ndarray, meta_dict: dict)-> None:
     """Get an ndarray of the image stack and extract each image to be saved as tif file.
     It uses multithreading to save each image."""
@@ -45,9 +41,7 @@ def process_img_array(array: np.ndarray, meta_dict: dict)-> None:
         frame,serie,z,_ = array_slice
         img_name = chan+'_s%02d'%(serie+1)+'_f%04d'%(frame+1)+'_z%04d'%(z+1)+'.tif'
         # Create save path and write the image
-        parent_folder = join(meta_dict['exp_path_list'][serie], 'Images')
-        if not exists(parent_folder):
-            mkdir(parent_folder)
+        parent_folder = save_dirs[serie]
         save_path = join(parent_folder,img_name)
         save_tif(array[array_slice],save_path,meta_dict['um_per_pixel'],meta_dict['interval_sec'])
     
@@ -55,6 +49,8 @@ def process_img_array(array: np.ndarray, meta_dict: dict)-> None:
     img_params_lst = get_img_params_lst(meta_dict)
     # Adjust array with missing dimension
     array = expand_array_dim(array,meta_dict['axes'])
+    # Create the save dir
+    save_dirs = [create_save_folder(exp_path, 'Images') for exp_path in meta_dict['exp_path_list']]    
     # Write the images
     with ThreadPoolExecutor() as executor:
         executor.map(write_array,img_params_lst)
@@ -62,28 +58,32 @@ def process_img_array(array: np.ndarray, meta_dict: dict)-> None:
 def process_img_obj(meta_dict: dict)-> None:
     """Get an ND2File obj map to extract each image to be saved as tif file.
     It uses multithreading to save each image."""
+    def write_nd2_obj(img_param: tuple)-> None:
+        # Unpack input data
+        chan, array_slice = img_param
+        frame,serie,z,chan_idx = array_slice
+        img_name = chan+'_s%02d'%(serie+1)+'_f%04d'%(frame+1)+'_z%04d'%(z+1)+'.tif'
+        # Get the image
+        index = get_frame(nd_obj,frame,serie,z)
+        img = nd_obj.read_frame(index)
+        # Create save path and write the image
+        parent_folder = save_dirs[serie]
+        save_path = join(parent_folder,img_name)
+        save_tif(img[chan_idx],save_path,meta_dict['um_per_pixel'],meta_dict['interval_sec'])
+    
     # Create all the names for the images+metadata
     img_params_lst = get_img_params_lst(meta_dict)
-    
-    # unableling user warnings, because we have to reuse the nd2_object for the different cores
-    warnings.filterwarnings("ignore", category=UserWarning) 
-    
+    # Create the save dir
+    save_dirs = [create_save_folder(exp_path, 'Images') for exp_path in meta_dict['exp_path_list']]
     # Generate input_data
-    img_obj = ND2File(meta_dict['img_path'])
-    input_data = [(img_obj,meta_dict,param) for param in img_params_lst]
-    
+    nd_obj = ND2File(meta_dict['img_path'])
     # Run multi-processing
-    with ProcessPoolExecutor() as executor: # nd2 file are messed up with multithreading
-        executor.map(write_nd2_obj,input_data)
-    img_obj.close()
-    
-    # enableing the warnings again
-    warnings.filterwarnings("default", category=UserWarning)
+    with ThreadPoolExecutor() as executor:
+        executor.map(write_nd2_obj,img_params_lst)
+    nd_obj.close()
 
-def get_img_params_lst(meta_dict: dict)-> list[dict]:
-    """Return a list of dict containing the slice indexes of all images,
-    that is the serie (position), frame, z-slice and channel of each images, as well as the name of the image
-    to be saved"""
+def get_img_params_lst(meta_dict: dict)-> list[tuple]:
+    """Return a list of tuples with the channel name and the array slice of the image."""
     # Create a name for each image
     img_params_lst = []
     for serie in range(meta_dict['n_series']):
@@ -108,35 +108,24 @@ def expand_array_dim(array: np.ndarray, axes: str)-> np.ndarray:
             array = np.expand_dims(array,axis=ax)
     return array
 
-
-
-def write_nd2_obj(img_data: list)-> None:
-    # Unpack img_data
-    img_obj,meta,param = img_data
-    serie,f,z,chan_idx = param['array_slice']
-    # Get the image
-    index = get_frame(nd_obj=img_obj, frame=f, serie=serie, z_slice=z)
-    img = img_obj.read_frame(index)
-    # Save
-    save_path = join(meta['exp_path_list'][param['serie']],'Images',param['img_name']+".tif")
-    save_tif(img[chan_idx],save_path,meta['um_per_pixel'],meta['interval_sec'])
-
 def get_frame(nd_obj: ND2File, frame:int, serie:int, z_slice:int)-> int:
     """Extract index of a specfic image from ND2File obj"""
     for entry in nd_obj.events():
         # Add missing axes
         if 'T Index' not in entry:
-            entry['T index'] = 0
+            entry['T Index'] = 0
         if 'P Index' not in entry:
             entry['P Index'] = 0
         if 'Z Index' not in entry:
-            entry['Z index'] = 0
+            entry['Z Index'] = 0
         # Extract Index
         if entry['Z Index'] == z_slice and entry['T Index'] == frame and entry['P Index'] == serie:
             return entry['Index']
 
 
 if __name__ == "__main__":
-    img_path = '/home/Test_images/tiff/Run2/c2z25t23v1_tif.tif'
-    
+    # Test tif image
+    # img_path = '/home/Test_images/tiff/Run2/c2z25t23v1_tif.tif'
+    # Test nd2 image
+    img_path = '/home/Test_images/nd2/Run4/c4z1t91v1.nd2'
     process_img(img_path)
--- pipeline/pre_process/preprocess_utils.py
+++ pipeline/pre_process/preprocess_utils.py
@@ -1,6 +1,7 @@
 
 from __future__ import annotations
-from os import PathLike
+from os import PathLike, mkdir
+from os.path import join, isdir
 from tifffile import imwrite
 import numpy as np
 
@@ -12,3 +13,12 @@ def save_tif(array: np.ndarray, save_path: PathLike, um_per_pixel: tuple[float,f
     """Save array as tif with metadata"""
     imagej_metadata = {'finterval':finterval, 'unit': 'um'}
     imwrite(save_path,array.astype(np.uint16),imagej=True,metadata=imagej_metadata,resolution=get_resolution(um_per_pixel))
+
+def create_save_folder(exp_path: PathLike, folder_name: str)-> PathLike:
+    save_folder = join(exp_path,folder_name)
+    if not isdir(save_folder):
+        print(f" ---> Creating saving folder: {save_folder}")
+        mkdir(save_folder)
+        return save_folder
+    print(f" ---> Saving folder already exists: {save_folder}")
+    return save_folder
